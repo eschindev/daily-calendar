f449676f8a190e3a61b3197f84ea3931eef6a8d2
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-04 19:49:38 -0700

    generated daily calendar with jQuery

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,4 +20,65 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentTime = dayjs();
+  $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
+
+  var calArea = $('#calArea');
+  var hourBlocks = [];
+  var hourName;
+  var hourTextArea;
+  var saveButton;
+  var key;
+  var saveButtonChild;
+  var hour;
+  var ampm;
+  var index;
+  for (i = 9; i <= 17; i++){
+    index = i - 9;
+    if (Math.floor(i / 12) === 0) {
+      ampm = "AM";  
+    } else {ampm = "PM"}
+    // create each row
+    if (i === 12) {
+      hour = 12;
+    } else {
+      hour = i % 12;
+    }
+    hourBlocks[index] = $("<div>").attr("id", `${hour}${ampm}`).addClass("row time-block");
+    // set each row as past, present, or future
+    if (i < parseInt(currentTime.format("H"))) {
+      hourBlocks[index].addClass("past");
+    } else if (i === parseInt(currentTime.format("H"))) {
+      hourBlocks[index].addClass("present");
+    } else {
+      hourBlocks[index].addClass("future");
+    }
+    // create content for each row
+    hourName = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(`${hour}${ampm}`);
+    hourTextArea = $("<textarea>").attr({
+      class: "col-8 col-md-10 description",
+      rows: "3"
+    });
+    // check localStorage for item with matching key and populate hourTextArea with value if found
+    key = `${hour}${ampm}-save`;
+    if (localStorage.key) {
+      hourTextArea.text(localStorage.getItem(key));
+    }
+    saveButton = $("<button>").attr({
+      id: `${hour}${ampm}-save`,
+      class: "btn saveBtn col-2 col-md-1",
+      "aria-label": "save"
+    });
+    saveButtonChild = $("<i>").attr({
+      class: "fas fa-save",
+      "aria-hidden": "true"
+    });
+    saveButton.append(saveButtonChild);
+    
+
+    hourBlocks[index].append(hourName);
+    hourBlocks[index].append(hourTextArea);
+    hourBlocks[index].append(saveButton);
+    calArea.append(hourBlocks[index]);
+  }
 });
